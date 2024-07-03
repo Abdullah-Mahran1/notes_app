48e84b7935dec85b650f15ef91e6bf2cebfdfa9d
Abdullah-Mahran <62305013+Abdullah-Mahran1@users.noreply.github.com>
2024-07-03 16:49:34 +0300

    cubit built, remaining BlocProvider & use

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Colors.cyan;
 const notesBox = 'notes_box';
+
+
+// Steps of creating a bloc:
+// 1. Write States, abstract class and a child class for each state
+// 2. Create a Cubit with modification methods
+// 3. Create BlocProvider & BlocBuilder
+// 4. Call BlocBuilding 
